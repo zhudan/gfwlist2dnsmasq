a5f9cc7ccd2c6da160f0ab527e30782bbcbdd0f2
zhudan <464033315@qq.com>
2022-06-30 17:06:26 +0800

    Update gfwlist2dnsmasq.sh

--- gfwlist2dnsmasq.sh
+++ gfwlist2dnsmasq.sh
@@ -12,7 +12,7 @@ MYDNSPORT=${3:-23453}
 IPSETNAME=${4:-dnsmasq_gfw}
 
 # GFWURL="https://ghproxy.com/https://raw.githubusercontent.com/Loukky/gfwlist-by-loukky/master/gfwlist.txt"
-GFWURL="https://ghproxy.com/https://raw.githubusercontent.com/hezhijie0327/GFWList2AGH/main/gfwlist2domain/blacklist_full.txt"
+GFWURL="https://raw.githubusercontent.com/hezhijie0327/GFWList2AGH/main/gfwlist2domain/blacklist_full.txt"
 GFWLIST_TMP_BASE64="/tmp/gfwlist.txt.base64"
 GFWLIST_TMP="/home/runner/work/publish/gfw.conf"
 # DNSMASQ_GFW="/jffs/configs/dnsmasq.d/gfw.conf"
